95e69b512216b7ccc69b22f42554ad44afbad636
Zhengmx27 <34853294+Zhengmx27@users.noreply.github.com>
2018-11-15 21:24:41 +0800

    Update cnn.py

--- cnn.py
+++ cnn.py
@@ -47,7 +47,7 @@ def loadTest_y(filePath):
 
 
 
-
+#加载数据
 
 train_x = []
 train_y = []
@@ -78,8 +78,7 @@ test_y = np.array(test_y)
 x = tf.placeholder(tf.float32,[None, 256])
 y_ = tf.placeholder(tf.float32, [None, 7])
 
-#----Weight Initialization---#
-#One should generally initialize weights with a small amount of noise for symmetry breaking, and to prevent 0 gradients
+
 def weight_variable(shape):
     initial = tf.truncated_normal(shape, stddev=0.1)
     return tf.Variable(initial)
@@ -88,9 +87,7 @@ def bias_variable(shape):
     return tf.Variable(initial)
 
 
-#Convolution and Pooling
-#Our convolutions uses a stride of one and are zero padded so that the output is the same size as the input.
-#Our pooling is plain old max pooling over 2x2 blocks
+
 def conv2d(x, W):
     return tf.nn.conv2d(x, W, strides=[1,1,1,1], padding='SAME')
 def max_pool_2x2(x):
@@ -99,25 +96,19 @@ def max_pool_2x2(x):
 #***********************在这里进行reshape 变成符合卷积神经网络的输入
 x_image = tf.reshape(x, [-1,16,16,1])
 
-#----first convolution layer----#
-#he convolution will compute 32 features for each 5x5 patch. Its weight tensor will have a shape of [5, 5, 1, 32].
-#The first two dimensions are the patch size,
-#the next is the number of input channels, and the last is the number of output channels.
+
 W_conv1 = weight_variable([5,5,1,32])
 
-#We will also have a bias vector with a component for each output channel.
+
 b_conv1 = bias_variable([32])
 
-#We then convolve x_image with the weight tensor, add the bias, apply the ReLU function, and finally max pool.
-#The max_pool_2x2 method will reduce the image size to 14x14.
 h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
 h_pool1 = max_pool_2x2(h_conv1)
 
 
 
 
-#----second convolution layer----#
-#The second layer will have 64 features for each 5x5 patch and input size 32.
+
 W_conv2 = weight_variable([5,5,32,64])
 b_conv2 = bias_variable([64])
 
@@ -126,8 +117,7 @@ h_pool2 = max_pool_2x2(h_conv2)
 
 
 
-#----fully connected layer----#
-#Now that the image size has been reduced to 4x4, we add a fully-connected layer with 1024 neurons to allow processing on the entire image
+
 W_fc1 = weight_variable([4*4*64, 1024])
 b_fc1 = bias_variable([1024])
 
@@ -136,10 +126,8 @@ h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat,W_fc1) + b_fc1)
 
 
 
-#-----dropout------#
-#To reduce overfitting, we will apply dropout before the readout layer.
-#We create a placeholder for the probability that a neuron's output is kept during dropout.
-#This allows us to turn dropout on during training, and turn it off during testing.
+#-dropout
+
 keep_prob = tf.placeholder(tf.float32)
 h_fc1_dropout = tf.nn.dropout(h_fc1, keep_prob)
 
@@ -159,7 +147,7 @@ accuracy = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(y_, 1), tf.argmax(y_conv, 1
 with tf.Session() as sess:
     tf.global_variables_initializer().run()
     for i in range(1000):
-        # 跑3000轮迭代，每次随机从训练样本中抽出50个进行训练
+       
         batch = ([], [])
         p = random.sample(range(2708), 100)
         for k in p:
